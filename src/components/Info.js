c383e47b75a8d11f4e234b39187cb7793c5e5fec
James Aduke <jamesodwuor4@gmail.com>
2022-02-09 08:41:14 +0300

    Add info complete styling for now

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ProfilePic from '../images/head-shot.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 
 
 function Info(){
@@ -8,18 +11,21 @@ function Info(){
         <div className="info">
             <img src={ProfilePic} alt="headshot" className="headshot"/>
 
-            <h3 className="info--name">Not James</h3>
-            <h5 className="info--title">Full Stack Developer</h5>
+            <h2 className="info--name">Not James Aduke</h2>
+            <h5 className="info--title">Frontend Developer</h5>
             <p className="info--web">jamesaduke.netlify.app</p>
 
             <div className="info--btn-group">
-                <button className="info--btn">
+                <button className="info--btn" id="email-btn">
+                    <FontAwesomeIcon icon={faEnvelope} className="email-icon" />
                     <a className="email-btn" href = "mailto: adukework@gmail.com">Email Me</a>
                 </button>
 
-                <button className="info--btn">
-                    <a className="email-btn" href = "https://linked.com/in/jamesaduke">LinkedIn</a>
+                <button className="info--btn" id="linkedin-btn">
+                    <i className="fa fa-linkedin" aria-hidden="true"/>
+                    <a className="linked-btn" href = "https://linked.com/in/jamesaduke">LinkedIn</a>
                 </button>
+
             </div>
         </div>
     )
